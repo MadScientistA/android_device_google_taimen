f1b72dd232f25a20e1033ca72b65743d34ebd7cf
MadScientistA <anmolzagrawal@gmail.com>
2021-04-30 05:59:18 +0200

    Added init to fix Safetynet CTS failure

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,1 +1,7 @@
 TARGET_KERNEL_CONFIG := lineageos_taimen_defconfig
+
+DEVICE_PATH := device/google/taimen
+
+# Vendor init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_taimen
+TARGET_RECOVERY_DEVICE_MODULES := libinit_taimen
